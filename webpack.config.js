f3e5df1acfa682621e48411e4209c2b0ae720033
Cat-Denton <willwdenton@gmail.com>
2021-03-26 22:32:06 -0700

    Update environment to allow .env to store API key

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -13,12 +14,14 @@ module.exports = {
     contentBase: './dist'
   },
   plugins: [
+    
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'jsEnvTemplate',
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv()
   ],
   module: {
     rules: [
